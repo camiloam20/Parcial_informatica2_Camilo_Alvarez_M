aaf8cf41dc8c48ba67444483d70fffa8f06c9c13
Camilo Alvarez Muñoz <camilo.alvarezm@udea.edu.co>
2020-05-10 11:56:35 -0500

    Primer Commit
    Inicio del parcial informatica 2

---
+++ PARCIAL_INFO2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+/* Parcial informatica 2- Desafio Evaluativo "Yo vine porque quise"
+ * Camilo Alvarez Muñoz - CC.1000874557
+*/
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
